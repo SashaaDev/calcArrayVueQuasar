6b985727c4b1431a56132a48547bc59e9f7ce7da
SashaaaDev <sashahutoranskij15@gmail.com>
2024-05-10 17:54:24 +0300

    created form for getting min, max and sum from array

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,87 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <q-page class="row justify-center items-center">
+    <q-card class="row justify-center q-pa-md">
+      <q-item class="q-pa-md">
+        <q-item-section class="q-pa-md" style="min-width: 500px">
+          <q-input class="q-mb-lg"
+                   v-model="text"
+                   filled
+                   type="textarea"
+                   :label=label
+          />
+          <q-select class="q-mb-lg" v-model="typeOperation" :options="options" label="Standard"/>
+          <q-input class="q-mb-lg" filled v-model="result" hint="Readonly" readonly/>
+          <q-card-section class="row justify-center">
+            <q-btn @click="request" label="Send" type="submit" color="primary"/>
+            <q-btn @click="text = ''; result = ''; errorMessage = ''; typeOperation = ''" label="Reset" type="reset"
+                   color="primary" flat class="q-ml-sm"/>
+          </q-card-section>
+          <q-item-label class="text-red row justify-center q-pa-md" v-if="errorMessage"> {{
+              errorMessage
+            }}
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-card>
+  </q-page>
 </template>
+
+<style>
+</style>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: 'HelloWorld'
+}
+</script>
+<script setup>
+import {ref} from 'vue'
+
+const options = ['min', 'max', 'sum']
+const typeOperation = ref('')
+const text = ref('')
+const label = ref('Enter your array')
+const result = ref('')
+const errorMessage = ref('')
+const request = () => {
+  fetch(`http://localhost/api/test/${typeOperation.value}`, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      'array': ifJSON(text.value)
+    })
+  })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        } else {
+          errorMessage.value = ''
+        }
+        return response.json();
+      })
+      .then(data => {
+        result.value = data;
+      })
+      .catch(error => {
+        if (typeOperation.value === '') {
+          errorMessage.value = 'Select operation'
+        } else {
+          console.error('There was an error!', error);
+          errorMessage.value = 'There was an error!'
+        }
+      });
+}
+
+function ifJSON(str) {
+  try {
+    return JSON.parse(str);
+  } catch (e) {
+    return str;
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+
