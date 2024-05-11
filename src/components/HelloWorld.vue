1f3657ac13e77229e89b6aec508d0094d221512f
SashaaaaDev <sashahutoranskij15@gmail.com>
2024-05-12 01:56:06 +0300

    added normal view of form on mobile

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,30 +1,22 @@
 <template>
   <q-page class="row justify-center items-center">
-    <q-card class="row justify-center q-pa-md">
-      <q-item class="q-pa-md">
-        <q-item-section class="q-pa-md" style="min-width: 500px">
-          <q-input class="q-mb-lg"
-                   v-model="text"
-                   filled
-                   type="textarea"
-                   :label=label
-          />
-          <q-select class="q-mb-lg" v-model="typeOperation" :options="options" label="Standard"/>
-          <q-input class="q-mb-lg" filled v-model="result" hint="Readonly" readonly/>
-          <q-card-section class="row justify-center">
-            <q-btn @click="request" label="Send" type="submit" color="primary"/>
-            <q-btn @click="text = ''; result = ''; errorMessage = ''; typeOperation = ''" label="Reset" type="reset"
-                   color="primary" flat class="q-ml-sm"/>
+    <q-card class="row justify-center q-pa-md" style="max-width: 400px">
+      <q-item class="q-pa-md wrap-sm">
+        <q-item-section class="q-pa-md" style="min-width: 300px">
+          <q-input class="q-mb-md" v-model="text" filled type="textarea" :label=label />
+          <q-select class="q-mb-md" v-model="typeOperation" :options="options" label="Standard" style="max-width: 250px"/> <!-- Зменшено ширину селекта -->
+          <q-input class="q-mb-md" filled v-model="result" hint="Readonly" readonly />
+          <q-card-section class="row justify-center q-mb-md">
+            <q-btn @click="request" label="Send" type="submit" color="primary" />
+            <q-btn @click="text = ''; result = ''; errorMessage = ''; typeOperation = ''" label="Reset" type="reset" color="primary" flat class="q-ml-md" />
           </q-card-section>
-          <q-item-label class="text-red row justify-center q-pa-md" v-if="errorMessage"> {{
-              errorMessage
-            }}
-          </q-item-label>
+          <q-item-label class="text-red row justify-center q-pa-md" v-if="errorMessage">{{ errorMessage }}</q-item-label>
         </q-item-section>
       </q-item>
     </q-card>
   </q-page>
 </template>
+
 
 <style>
 </style>
